a7fda474f41d7cc390693652be9922955f95a866
Daniel Prado <dfe.prado@gmail.com>
2023-06-28 09:01:25 -0300

    Define the listFinancingFiles()

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"math"
 	"os"
+	"strings"
 )
 
 type InvalidInterestError float64
@@ -25,6 +26,19 @@ var DFINANCE_DIR string = fmt.Sprintf("%s/%s", os.Getenv("HOME"), ".dfinance")
 
 func main() {
 	createDFinanceHome()
+
+	financingFiles, err := listFinancingFiles()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(financingFiles) == 0 {
+		log.Fatal("There is no financing file.")
+	}
+
+	for _, fin := range financingFiles {
+		fmt.Println("Financing file:", fin)
+	}
 }
 
 func createDFinanceHome() {
@@ -36,6 +50,27 @@ func createDFinanceHome() {
 	} else {
 		log.Default().Printf("DFinance home created at %v", DFINANCE_DIR)
 	}
+}
+
+func listFinancingFiles() ([]string, error) {
+	files, err := os.ReadDir(DFINANCE_DIR)
+	if err != nil {
+		return nil, err
+	}
+
+	// Filter .dfin files only
+	var financingFiles []string
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		if idx := strings.Index(file.Name(), ".dfin"); idx != -1 {
+			financingFiles = append(financingFiles, file.Name())
+		}
+	}
+
+	return financingFiles, nil
 }
 
 func CreateFinancing(cost float64, installmentCount uint16, interest float64) (*Financing, error) {
